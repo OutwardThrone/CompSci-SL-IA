f064523b413f3328e8a3e90f4b18c2e827d9d181
Vibhav Peri <vibhavperi@gmail.com>
2020-03-13 14:42:21 -0700

    Update courseinfo.js

--- pages/courseinfo.js
+++ pages/courseinfo.js
@@ -2,36 +2,44 @@ import Base from "../components/base";
 import { firestore } from "../firebase";
 import Course from "../classes/course";
 import Link from "next/link";
+import User from "../classes/user";
+import { Badge } from "reactstrap";
 
-const onCourseClick = function(course) {
-
+const CourseInfo = props => {
+    const u = new User(props.currentUser.email, props.currentUser.password, props.currentUser.name)
+    
+    return (
+        <>
+            <div className="course-info">
+                {props.courseInfo.map(courseAndInCourse => {
+                    return (
+                        <Link href="/courses/[id]" as={`/courses/${courseAndInCourse[0].id}`} key={courseAndInCourse[0].id}>
+                            <div className="course" id={courseAndInCourse[0].id} >
+                                <div className="course-name">{courseAndInCourse[0].name}  {courseAndInCourse[1] ? <Badge pill color="success">Enrolled</Badge> : <></>}</div>
+                                <p>{courseAndInCourse[0].description || "no desc"}</p>
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
 
-const CourseInfo = props => (
-    <>
-        <div className="course-info">
-            {props.availableCourses.map(course => {
-                return (
-                    <Link href="/courses/[id]" as={`/courses/${course.id}`}>
-                        <div className="course" id={course.id} >
-                            <div className="course-name">{course.name}</div>
-                            <p>{course.description || "no desc"}</p>
-                        </div>
-                    </Link>
-                )
-            })}
-        </div>
-    </>
-)
-
-// CourseInfo.getInitialProps = async function(ctx) {
-//     let availableCourses = []
-//     await firestore.collection('availableCourses').withConverter(Course.courseConverter).get().then(docs => {
-//         docs.forEach(doc => {
-//             availableCourses.push(doc.data())
-//         })
-//     })
-//     return {availableCourses: availableCourses}
-// }
+CourseInfo.getInitialProps = async function(ctx) {
+    let courseInfo = []
+    if (ctx.currentUser) {
+        const u = new User(ctx.currentUser.email, ctx.currentUser.password, ctx.currentUser.name)
+        for (let course of ctx.availableCourses) {
+            let isInCourse = await u.isInCourse(course)
+            courseInfo.push([course, isInCourse])
+        }
+    } else {
+        for (let course of ctx.availableCourses) {
+            courseInfo.push([course, false])
+        }
+    }
+    return {courseInfo: courseInfo}
+}
 
 export default CourseInfo
